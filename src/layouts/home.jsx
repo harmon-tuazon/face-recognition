a87c9236527478efe98993a9cf592bf9e3e409cc
harmon-tuazon <harmontuazon@gmail.com>
2023-10-24 21:31:32 +0800

    finished with API. To work on detection box next

--- src/layouts/home.jsx
+++ src/layouts/home.jsx
@@ -1,10 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ImageLinkForm from "../component/imageLinkForm";
 import FaceRecognition from "../component/faceRecognition";
 
 const Home = () => {
     const [imageURL, setImageURL] = useState('')
     const [linkInput, setLinkInput] = useState('')
+
+    useEffect(() => {
+        if (imageURL === "") return;
+
+        const fetchClarifaiRequest = async () => {
+            const response = await fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs",returnClarifaiReqeuestOption(linkInput));
+            const data = await response.json();
+    
+            try {
+                console.log(data.outputs[0].data.regions[0].region_info.bounding_box)
+            }
+            catch(err) {
+                console.log(err)
+            }} 
+            fetchClarifaiRequest() 
+        }, [imageURL])
+
+    const returnClarifaiReqeuestOption = (imageURL) => {
+        const PAT = '4d1e9d6b2893438fbc2b71eb63005665';
+        const USER_ID = 'pqouo73fxd7a';       
+        const APP_ID = 'face-brain-recognition';
+        const MODEL_ID = 'face-detection';
+        const IMAGE_URL = imageURL;
+
+        const raw = JSON.stringify({
+            "user_app_id": {
+                "user_id": USER_ID,
+                "app_id": APP_ID
+            },
+            "inputs": [
+                {
+                    "data": {
+                        "image": {
+                            "url": IMAGE_URL
+                        }
+                    }
+                }
+            ]
+        });
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Key ' + PAT
+            },
+            body: raw
+        }
+        return requestOptions;
+    }
 
     const handleChange = (event) => {
         setLinkInput(event.target.value)
@@ -13,8 +63,6 @@ const Home = () => {
     const handleClick = (event) => {
         event.preventDefault();
         setImageURL(linkInput);
-
-        /*this is where the API stuff happens*/
     }
 
 
