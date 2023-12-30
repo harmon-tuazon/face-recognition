c382621abc7fc831df714fc63222ae924e61a0f6
Harmon Tuazon <harmontuazon@gmail.com>
2023-12-30 11:35:17 -0500

    tested frontend & backend connection via fetch api

--- src/layouts/register.jsx
+++ src/layouts/register.jsx
@@ -1,34 +1,90 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const defaultCredentials = {
+                            fullname: '',
+                            email: '',
+                            password: '', 
+                                         }
 
 const Register = () => {
+    const navigate = useNavigate()
+    const [regCredentials, setRegCredentials] = useState(defaultCredentials)
+
+    const handleCredentialChange = (event) => {
+        const {name, value} = event.target
+    
+        setRegCredentials({
+          ...regCredentials,
+          [name]:value
+        });
+    }
+
+    const handleSubmitReg = async () => {
+        const {fullname, email, password} = regCredentials
+
+        try {
+           const response = await fetch('http://localhost:3000/register', {
+                                   method: 'post',
+                                   headers: {"Content-Type": "application/json"},
+                                   body: JSON.stringify({
+                                           fullname: fullname, 
+                                           email: email,
+                                           password: password
+                                   })})
+           const data = await response.json()
+           
+           if (data.status === 201) {
+               navigate("/")
+           }
+       } catch(err) {
+           console.log(err)
+       }
+   } 
+    
 
     return (
-        <article class="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw8 center shadow-4">
-            <main class="pa4 black-80">
-                <form class="measure center"  action="/register" method="post">
+        <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw8 center shadow-4">
+            <main className="pa4 black-80">
+                <form className="measure center"  action="/register" method="post">
 
-                    <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-                        <legend class="f4 fw6 ph0 mh0">Register</legend>
-                        <div class="mt3">
-                            <label class="db fw6 lh-copy f6" for="fullname">Full Name</label>
-                            <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" id="fullname"/>
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f4 fw6 ph0 mh0">Register</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="fullname">Full Name</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                   type="text" 
+                                   id="fullname"
+                                   name="fullname"
+                                   onInput={handleCredentialChange}/>
                         </div>
-                        <div class="mt3">
-                            <label class="db fw6 lh-copy f6" for="email-address">Email</label>
-                            <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" id="email-address"/>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="email">Email</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                   type="email" 
+                                   id="email"
+                                   name="email"
+                                   onInput={handleCredentialChange}/>
                         </div>
-                        <div class="mv3">
-                            <label class="db fw6 lh-copy f6" for="password">Password</label>
-                            <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" id="password"/>
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                   type="password" 
+                                   id="password"
+                                   name="password"
+                                   onInput={handleCredentialChange}/>
                         </div>
                     </fieldset>
 
-                    <div class="">
-                        <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="button" value="Sign in"/>
+                    <div className="">
+                        <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                               type="button" 
+                               value="Sign in"
+                               onClick={handleSubmitReg}/>
                     </div>
 
-                    <div class="lh-copy mt3">
-                        <Link to="/sign-in" class="f6 link dim black db">Already have an account? Sign-in</Link>
+                    <div className="lh-copy mt3">
+                        <Link to="/sign-in" className="f6 link dim black db">Already have an account? Sign-in</Link>
                     </div>
                     
                 </form>
--- src/layouts/signin.jsx
+++ src/layouts/signin.jsx
@@ -23,23 +23,25 @@ const Signin = () => {
     }
     
     const handleSubmitSignIn = async () => {
-         const response = await fetch('http://localhost:3000/signin', {
+        const {email, password} = logInCredentials
+
+         try {
+            const response = await fetch('http://localhost:3000/signin', {
                                     method: 'post',
                                     headers: {"Content-Type": "application/json"},
                                     body: JSON.stringify({
-                                            email: email,
-                                            password: password
-                                    })})
-         const user = await response.json()
+                                                          email: email,
+                                                          password: password                                   
+                                                                             })
+                                })
+            const data = await response.json()
 
-         try {
-            setIsSignedOut(false)
-
-            if (user) {
+            if (response.status !== 400) {
+                setIsSignedOut(false)
                 navigate("/")
             }
         } catch(err) {
-            throw new Error(err)
+           console.log(err)
         }
     } 
 
@@ -47,23 +49,23 @@ const Signin = () => {
         <>
         {isSignedOut ? 
         
-        <article class="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw8 center shadow-4">
-            <main class="pa4 black-80">
-                <form class="measure center" action="/signin" method="post">
+        <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw8 center shadow-4">
+            <main className="pa4 black-80">
+                <form className="measure center" action="/signin" method="post">
 
-                    <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-                        <legend class="f4 fw6 ph0 mh0">Sign In</legend>
-                        <div class="mt3">
-                            <label class="db fw6 lh-copy f6" for="email">Email</label>
-                            <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="email">Email</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                                    type="email"   
                                    id="email"
                                    name="email"
                                    onInput={handleCredentialChange}/>
                         </div>
-                        <div class="mv3">
-                            <label class="db fw6 lh-copy f6" for="password">Password</label>
-                            <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
                                    type="password" 
                                    id="password"
                                    name="password"
@@ -71,15 +73,15 @@ const Signin = () => {
                         </div>
                     </fieldset>
 
-                    <div class="">
-                        <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                    <div className="">
+                        <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                                type="button" 
                                value="Sign in" 
                                onClick={handleSubmitSignIn}/>
                     </div>
 
-                    <div class="lh-copy mt3">
-                        <Link to="/register" class="f6 link dim black db">Don't have an account yet? Register</Link>
+                    <div className="lh-copy mt3">
+                        <Link to="/register" className="f6 link dim black db">Don't have an account yet? Register</Link>
                     </div>
 
                 </form>
@@ -88,13 +90,13 @@ const Signin = () => {
 
         :   
     
-        <article class="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw8 center shadow-4">
-            <main class="pa4 black-80">
-                <form class="measure center">
-                    <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-                        <legend class="f4 fw6 ph0 mh0">We hate to see you leave...</legend>
-                        <div class="mt4">
-                            <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="button" value="Sign Out"/>
+        <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw8 center shadow-4">
+            <main className="pa4 black-80">
+                <form className="measure center">
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f4 fw6 ph0 mh0">We hate to see you leave...</legend>
+                        <div className="mt4">
+                            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="button" value="Sign Out"/>
                         </div>
                     </fieldset>
                 </form>
